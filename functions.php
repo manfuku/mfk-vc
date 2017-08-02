dd17682522191e58e1de818a889eb1cb06fbf0ef
manfuku <manfuku@users.noreply.github.com>
2017-08-02 23:07:15 +0800

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+show_admin_bar(false);
+
+/* function use_cdn_jquery() {
+wp_deregister_script('jquery');
+wp_enqueue_script('jquery', 'https://cdn.jsdelivr.net/jquery/2.2.4/jquery.min.js', array(), null, true);
+}
+add_action('wp_enqueue_scripts', 'use_cdn_jquery'); */
